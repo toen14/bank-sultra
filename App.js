2530ca656bb1e9749bcbe5bd719a59f395913488
Ardiman Tando <w3r3n3@gmail.com>
2022-04-04 13:14:59 +0800

    set fonts

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
 import "react-native-gesture-handler"; // https://reactnavigation.org/docs/drawer-navigator#installation
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
+import AppLoading from "expo-app-loading";
+import { useFonts } from "expo-font";
+
 import DrawerNavigation from "./src/navigations/drawer-navigation";
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Rubik: require("./assets/fonts/Rubik.ttf"),
+    "Rubik-Bold": require("./assets/fonts/Rubik-Bold.ttf"),
+    "Rubik-Italic": require("./assets/fonts/Rubik-Italic.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <NavigationContainer>
       <DrawerNavigation />
