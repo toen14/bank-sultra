2dc7765d00a7b23f2e9fc0ffe3c773f363ae79c8
Ardiman Tando <w3r3n3@gmail.com>
2022-04-14 16:20:28 +0800

    add more faker data

--- fake-data/faker.mjs
+++ fake-data/faker.mjs
@@ -1,5 +1,6 @@
 import { faker } from "@faker-js/faker";
 import fs from "fs";
+import { cwd } from "process";
 
 const fakerDebitor = [];
 
@@ -14,8 +15,56 @@ for (let i = 0; i < 50; i++) {
     tanggal_penyerahan: faker.datatype.datetime(),
     tanggal_berakhir: faker.datatype.datetime(),
     status: faker.random.arrayElement(["done", "progress", "pending"]),
+    alamat: faker.random.arrayElement(["Raha", "Kendari", "Unaha", "Buton"]),
     id: i + 1,
   });
 }
 
-fs.writeFileSync("debitor.json", JSON.stringify(fakerDebitor));
+fs.writeFileSync(
+  `${cwd()}/fake-data/debitor.json`,
+  JSON.stringify(fakerDebitor)
+);
+
+const fakerBranchs = [];
+const branchs = [
+  "Cabang Raha",
+  "Cabang Kendari",
+  "Cabang Unaha",
+  "Cabang Buton",
+  "Cabang Bombana",
+  "Buton Selatan",
+  "Cabang Kolaka",
+  "Cabang Konawe",
+  "Cabang Konawe Kepulawan",
+  "Cabang Muna Barat",
+  "Cabang Baubau",
+];
+
+for (let i = 0; i < branchs.length; i++) {
+  fakerBranchs.push({
+    branch: branchs[i],
+    id: i + 1,
+  });
+}
+
+fs.writeFileSync(
+  `${cwd()}/fake-data/branch.json`,
+  JSON.stringify(fakerBranchs)
+);
+
+const fakerUsers = [];
+
+for (let i = 0; i < 50; i++) {
+  fakerUsers.push({
+    role: faker.random.arrayElement([
+      "Administrator",
+      "Admin Pusat",
+      "Apraisal",
+      "Notaris",
+    ]),
+    id: i + 1,
+    name: faker.name.findName(),
+  });
+}
+
+fs.writeFileSync(`${cwd()}/fake-data/user.json`, JSON.stringify(fakerUsers));
