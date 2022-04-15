b73d1d5169e2d947dd34aee5f4a6db71d575cafa
Ardiman Tando <w3r3n3@gmail.com>
2022-04-15 11:36:12 +0800

    user-screen: update component & access server

--- src/screens/user-screen.js
+++ src/screens/user-screen.js
@@ -1,35 +1,98 @@
-import React from "react";
-import { View, StyleSheet, Dimensions, ScrollView } from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  StyleSheet,
+  Dimensions,
+  FlatList,
+  RefreshControl,
+} from "react-native";
 
 import Search from "../components/search";
 import CardUser from "../components/card-user";
 import Bottom from "../components/bottom";
+import ErrorOverlay from "../components/UI/ErrorOverlay";
+import LoadingOverlay from "../components/UI/LoadingOverlay";
+import { baseUrl } from "../constants/base-url";
 
 export default function User(props) {
+  const [users, setUsers] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const getUsers = () => {
+    setIsLoading(true);
+    fetch(`${baseUrl}/users/?page=${currentPage}&limit=10`)
+      .then((res) => res.json())
+      .then((res) => {
+        setUsers([...users, ...res]);
+      })
+      .catch(() => setError("Could not fetch users!"))
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
+
+  const renderItem = ({ item, index }) => {
+    return (
+      <View style={styles.cardBranchContainer}>
+        <CardUser no={index + 1} name={item.name} role={item.role} />
+      </View>
+    );
+  };
+
+  const renderLoader = () => {
+    return isLoading ? <LoadingOverlay /> : null;
+  };
+
+  const loadMoreItem = () => {
+    setCurrentPage(currentPage + 1);
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, [currentPage]);
+
+  if (error && !isLoading) {
+    return <ErrorOverlay message={error} />;
+  }
+
+  if (isLoading && !users.length) {
+    return <LoadingOverlay />;
+  }
+
   return (
     <View style={styles.container}>
-      <View style={{ width: "100%", flex: 1 }}>
-        <ScrollView>
-          <View style={styles.header}>
-            <View style={styles.search}>
-              <Search />
-            </View>
+      <View
+        style={{
+          width: "100%",
+          flex: 1,
+        }}
+      >
+        <View style={styles.header}>
+          <View style={styles.search}>
+            <Search />
           </View>
-          <View style={{ alignSelf: "center" }}>
-            <View style={styles.cardBranchContainer}>
-              <CardUser no="1" name="Rizik" role="Administrator" />
-            </View>
-            <View style={styles.cardBranchContainer}>
-              <CardUser no="2" name="Samarah" role="Admin Pusat" />
-            </View>
-            <View style={styles.cardBranchContainer}>
-              <CardUser no="3" name="Lisa" role="Apraisal" />
-            </View>
-            <View style={styles.cardBranchContainer}>
-              <CardUser no="4" name="Munarman" role="Notaris" />
-            </View>
-          </View>
-        </ScrollView>
+        </View>
+        <FlatList
+          style={{ width: "100%" }}
+          data={users}
+          renderItem={renderItem}
+          maxToRenderPerBatch={10}
+          keyExtractor={(item) => item.id}
+          ListFooterComponent={renderLoader}
+          onEndReached={loadMoreItem}
+          onEndReachedThreshold={5}
+          refreshControl={
+            <RefreshControl
+              refreshing={false}
+              onRefresh={() => {
+                setUsers([]);
+                setCurrentPage(1);
+              }}
+            />
+          }
+        />
       </View>
 
       <View
@@ -66,5 +129,6 @@ export const styles = StyleSheet.create({
     width: "90%",
     alignItems: "center",
     justifyContent: "center",
+    alignSelf: "center",
   },
 });
