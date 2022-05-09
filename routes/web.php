4ce0d754af4cdacbd879e9902c09633bc8225b7b
Ardiman Tando <w3r3n3@gmail.com>
2022-05-09 23:55:14 +0800

    web: added auth

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,8 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/dashboard', [\App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard');
+    Route::resource('/users', \App\Http\Controllers\UserController::class);
+    Route::resource('/debitors', \App\Http\Controllers\DebitorController::class);
+});
 
-Route::resource('/users', \App\Http\Controllers\UserController::class);
-
-Route::resource('/debitors', \App\Http\Controllers\DebitorController::class);
+require __DIR__ . '/auth.php';
