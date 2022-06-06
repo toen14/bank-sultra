7b0e54be8723456bb49ff9aa528e5861ba2bc668
Ardiman Tando <w3r3n3@gmail.com>
2022-06-06 22:12:51 +0800

    feat: real users

--- src/Home/Users/User.tsx
+++ src/Home/Users/User.tsx
@@ -1,30 +1,82 @@
-import React, { useState } from "react";
+import React, {
+  memo,
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 import { SearchBar } from "@rneui/themed";
-import { Platform, FlatList } from "react-native";
-import { NativeBaseProvider } from "native-base";
+import { Platform, FlatList, RefreshControl } from "react-native";
+import { HStack, NativeBaseProvider, Spinner } from "native-base";
+import axios from "axios";
 
 import { HomeNavigationProps } from "../../components/Navigation";
 import { Box, Header } from "../../components";
+import { baseUrl } from "../../constants/base-url";
+import { AuthContext } from "../../Authentication/store/AuthContex";
 
 import List, { ListProps } from "./List";
 
-type Users = {
+type User = {
   id: number;
-} & Omit<ListProps, "no">;
+} & ListProps;
 
-const users: Users[] = [];
-
-let number = 1;
-for (const later of "abcdefghijkl".toUpperCase()) {
-  users.push({
-    name: later,
-    id: number++,
-    role: number % 2 ? "notaris" : "apraisal",
-  });
-}
+const MemoList = memo(
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  ({ id, name, role, no }: User) => {
+    return <List role={role} no={no} name={name} />;
+  },
+  () => true
+);
 
 const User = ({ navigation }: HomeNavigationProps<"User">) => {
   const [search, setSearch] = useState("");
+  const [users, setUsers] = useState<User[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isCanFetch, setIsCanFetch] = useState(true);
+
+  const [page, setPage] = useState(1);
+
+  const authCtx = useContext(AuthContext);
+
+  function fetchData(p: number) {
+    setIsLoading(true);
+    return axios
+      .get(`${baseUrl}/users?limit=10&page=${p}`, {
+        headers: {
+          Authorization: `Bearer ${authCtx.currentUser.token}`,
+        },
+      })
+      .then((res) => {
+        if (res.data.data.length) {
+          setUsers((prev) => [...prev, ...res.data.data]);
+        } else {
+          setIsCanFetch(false);
+        }
+      })
+      .catch((e) => console.log("err", e))
+      .finally(() => setIsLoading(false));
+  }
+
+  useEffect(() => {
+    fetchData(1);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const renderLoader = () => {
+    return isLoading ? (
+      <HStack space={8} justifyContent="center" alignItems="center">
+        <Spinner size="lg" />
+      </HStack>
+    ) : null;
+  };
+
+  const loadMoreItem = async () => {
+    if (isCanFetch) {
+      setPage((previousPage) => previousPage + 1);
+      await fetchData(page);
+    }
+  };
 
   return (
     <NativeBaseProvider>
@@ -47,10 +99,32 @@ const User = ({ navigation }: HomeNavigationProps<"User">) => {
           />
           <FlatList
             data={users}
-            renderItem={({ item, index }) => (
-              <List role={item.role} no={index + 1} name={item.name} />
+            ListFooterComponent={renderLoader}
+            onEndReached={loadMoreItem}
+            onEndReachedThreshold={0}
+            renderItem={useCallback(
+              ({ item, index }) => (
+                <MemoList
+                  role={item.role}
+                  no={index + 1}
+                  name={item.name}
+                  id={item.id}
+                />
+              ),
+              []
             )}
-            keyExtractor={(item) => item.id.toString()}
+            keyExtractor={(_, index) => index.toString()}
+            refreshControl={
+              <RefreshControl
+                refreshing={false}
+                onRefresh={() => {
+                  setUsers([]);
+                  setPage(1);
+                  setIsCanFetch(true);
+                  setTimeout(() => fetchData(1), 500);
+                }}
+              />
+            }
           />
         </Box>
       </Box>
