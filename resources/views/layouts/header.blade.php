808f2937c39da0f1ffb3e5e1134993eced70749b
Ardiman Tando <w3r3n3@gmail.com>
2022-06-19 10:24:48 +0800

    feat: update header template

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,8 +1,8 @@
 <head>
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <title>Atlantis Lite - Bootstrap 4 Admin Dashboard</title>
+    <title>BANK SULTRA - Sistem Informasi</title>
     <meta content="width=device-width, initial-scale=1.0, shrink-to-fit=no" name="viewport" />
-    <link rel="icon" href="{{ asset('/assets/img/icon.ico') }}" type="image/x-icon" />
+    <link rel="icon" href="{{ asset('/assets/img/banksultra-ico.png') }}" type="image/x-icon" />
 
     <!-- Fonts and icons -->
     <script src="{{ asset('assets/js/plugin/webfont/webfont.min.js') }}"></script>
