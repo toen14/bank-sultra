2261e69ca6f9d0348e53f81a66015867bae1a439
Ardiman Tando <w3r3n3@gmail.com>
2022-07-29 19:57:47 +0800

    feat: hidde backup on sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,85 +1,77 @@
 <!-- Sidebar -->
 <div class="sidebar sidebar-style-2">
-  <div class="sidebar-wrapper scrollbar scrollbar-inner">
-    <div class="sidebar-content">
-      <div class="user">
-        <div class="info">
-          <a class="ml-2" data-toggle="collapse" href="#collapseExample" aria-expanded="true">
-            <span>
-              {{auth()->user()->name}}
-              <span class="user-level">{{auth()->user()->role}}</span>
-            </span>
-          </a>
-          <div class="clearfix"></div>
+    <div class="sidebar-wrapper scrollbar scrollbar-inner">
+        <div class="sidebar-content">
+            <div class="user">
+                <div class="info">
+                    <a class="ml-2" data-toggle="collapse" href="#collapseExample" aria-expanded="true">
+                        <span>
+                            {{ auth()->user()->name }}
+                            <span class="user-level">{{ auth()->user()->role }}</span>
+                        </span>
+                    </a>
+                    <div class="clearfix"></div>
+                </div>
+            </div>
+            <ul class="nav nav-primary">
+                <li class="nav-item" id="li-dashboard">
+                    <a href=" {{ route('dashboard') }} " class="collapsed" aria-expanded="false">
+                        <i class="fas fa-home"></i>
+                        <p>Dashboard</p>
+                    </a>
+                </li>
+                <li class="nav-section">
+                    <span class="sidebar-mini-icon">
+                        <i class="fa fa-ellipsis-h"></i>
+                    </span>
+                    <h4 class="text-section">Components</h4>
+                </li>
+                <li class="nav-item  submenu">
+                    <a data-toggle="collapse" href="#data-master">
+                        <i class="fas fa-layer-group"></i>
+                        <p>Data Master</p>
+                        <span class="caret"></span>
+                    </a>
+                    <div class="collapse" id="data-master">
+                        <ul class="nav nav-collapse">
+                            <li class="">
+                                <a href=" {{ route('debitors.index') }} ">
+                                    <span class="sub-item">Data Debitur</span>
+                                </a>
+                            </li>
+                            <li class="">
+                                <a href=" {{ route('users.index') }} ">
+                                    <span class="sub-item">Data Pengguna</span>
+                                </a>
+                            </li>
+                            <li class="">
+                                <a href=" {{ route('branches.index') }} ">
+                                    <span class="sub-item">Data Cabang</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+                @if (Auth::user()->role === 'Administrator')
+                    <li class="nav-item  submenu">
+                        <a data-toggle="collapse" href="#administrasi">
+                            <i class="fas fa-cogs"></i>
+                            <p>Administrasi</p>
+                            <span class="caret"></span>
+                        </a>
+                        <div class="collapse" id="administrasi">
+                            <ul class="nav nav-collapse">
+                                <li class="">
+                                    <a href="{{ route('backup.index') }}">
+                                        <span class="sub-item">Backups</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+                @endif
+            </ul>
         </div>
-      </div>
-      <ul class="nav nav-primary">
-        <li class="nav-item" id="li-dashboard">
-          <a href=" {{ route('dashboard') }} " class="collapsed" aria-expanded="false">
-            <i class="fas fa-home"></i>
-            <p>Dashboard</p>
-          </a>
-        </li>
-        <li class="nav-section">
-          <span class="sidebar-mini-icon">
-            <i class="fa fa-ellipsis-h"></i>
-          </span>
-          <h4 class="text-section">Components</h4>
-        </li>
-        <li class="nav-item  submenu">
-          <a data-toggle="collapse" href="#data-master">
-            <i class="fas fa-layer-group"></i>
-            <p>Data Master</p>
-            <span class="caret"></span>
-          </a>
-          <div class="collapse" id="data-master">
-            <ul class="nav nav-collapse">
-              <li class="">
-                <a href=" {{ route('debitors.index') }} ">
-                  <span class="sub-item">Data Debitur</span>
-                </a>
-              </li>
-              <li class="">
-                <a href=" {{ route('users.index') }} ">
-                  <span class="sub-item">Data Pengguna</span>
-                </a>
-              </li>
-              <li class="">
-                <a href=" {{ route('branches.index') }} ">
-                  <span class="sub-item">Data Cabang</span>
-                </a>
-              </li>
-            </ul>
-          </div>
-        </li>
-        <li class="nav-item  submenu">
-          <a data-toggle="collapse" href="#administrasi">
-            <i class="fas fa-cogs"></i>
-            <p>Administrasi</p>
-            <span class="caret"></span>
-          </a>
-          <div class="collapse" id="administrasi">
-            <ul class="nav nav-collapse">
-              <li class="">
-                <a href="{{route('backup.index')}}">
-                  <span class="sub-item">Backups</span>
-                </a>
-              </li>
-              {{-- <li class="">
-                <a href="#">
-                  <span class="sub-item">History Logs</span>
-                </a>
-              </li>
-              <li class="">
-                <a href="#">
-                  <span class="sub-item">Restore</span>
-                </a>
-              </li> --}}
-            </ul>
-          </div>
-        </li>
-      </ul>
     </div>
-  </div>
 </div>
 <!-- End Sidebar -->
