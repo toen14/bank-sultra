305eeb81f95afa5ec153c5765e758a51e2fcf43d
Ardiman Tando <w3r3n3@gmail.com>
2022-11-01 17:46:34 +0800

    feat: update notification

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -3,6 +3,17 @@
         background-color: #001244 !important;
     }
 </style>
+@php
+    $user = auth()->user();
+    $notifications = \App\Models\Notification::with(['note', 'note.user'])
+        ->whereHas('note', function ($query) use ($user) {
+            $query->where('user_id', '!=', $user->id);
+        })
+        ->where('user_id', $user->id)
+        ->where('status', 'unread')
+        ->orderBy('id', 'DESC')
+        ->get();
+@endphp
 <div class="main-header">
     <!-- Logo Header -->
     <div class="logo-header"
@@ -34,7 +45,7 @@
 
         <div class="container-fluid">
             <ul class="navbar-nav topbar-nav ml-md-auto align-items-center">
-                <li class="nav-item dropdown hidden-caret">
+                {{-- <li class="nav-item dropdown hidden-caret">
                     <a class="nav-link dropdown-toggle nav-icon" href="#" id="messageDropdown" role="button"
                         data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         <i class="fa fa-envelope text-white"></i>
@@ -105,57 +116,34 @@
                                     class="fa fa-angle-right"></i> </a>
                         </li>
                     </ul>
-                </li>
+                </li> --}}
                 <li class="nav-item dropdown hidden-caret">
                     <a class="nav-link dropdown-toggle nav-icon" href="#" id="notifDropdown" role="button"
                         data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         <i class="fa fa-bell text-white"></i>
-                        <span class="notification">4</span>
+                        <span class="notification font-weight-bold">{{ $notifications->count() }}</span>
                     </a>
                     <ul class="dropdown-menu notif-box animated fadeIn" aria-labelledby="notifDropdown">
                         <li>
-                            <div class="dropdown-title">You have 4 new notification</div>
+                            <div class="dropdown-title">You have {{ $notifications->count() }} new notifications</div>
                         </li>
                         <li>
                             <div class="notif-center">
-                                <a href="#">
-                                    <div class="notif-icon notif-primary"> <i class="fa fa-user-plus"></i> </div>
-                                    <div class="notif-content">
-                                        <span class="block">
-                                            New user registered
+                                @foreach ($notifications as $notification)
+                                    <a href="#">
+                                        <span
+                                            class="avatar-sm rounded-circle d-flex justify-content-center align-items-center text-primary font-weight-bold m-2"
+                                            style="background-color: rgb(202, 202, 202);">
+                                            {{ iconName($notification->note->user->name) }}
                                         </span>
-                                        <span class="time">5 minutes ago</span>
-                                    </div>
-                                </a>
-                                <a href="#">
-                                    <div class="notif-icon notif-success"> <i class="fa fa-comment"></i> </div>
-                                    <div class="notif-content">
-                                        <span class="block">
-                                            Rahmad commented on Admin
-                                        </span>
-                                        <span class="time">12 minutes ago</span>
-                                    </div>
-                                </a>
-                                <a href="#">
-                                    <div class="notif-img">
-                                        <img src="../../assets/img/profile2.jpg" alt="Img Profile">
-                                    </div>
-                                    <div class="notif-content">
-                                        <span class="block">
-                                            Reza send messages to you
-                                        </span>
-                                        <span class="time">12 minutes ago</span>
-                                    </div>
-                                </a>
-                                <a href="#">
-                                    <div class="notif-icon notif-danger"> <i class="fa fa-heart"></i> </div>
-                                    <div class="notif-content">
-                                        <span class="block">
-                                            Farrah liked Admin
-                                        </span>
-                                        <span class="time">17 minutes ago</span>
-                                    </div>
-                                </a>
+                                        <div class="notif-content">
+                                            <span class="block">
+                                                {{ $notification->note->user->name }} membuat note
+                                            </span>
+                                            <span class="time">{{ $notification->created_at->diffForHumans() }}</span>
+                                        </div>
+                                    </a>
+                                @endforeach
                             </div>
                         </li>
                         <li>
@@ -227,8 +215,7 @@
                     </div>
                 </li>
                 <li class="nav-item dropdown hidden-caret">
-                    <a class="dropdown-toggle profile-pic" data-toggle="dropdown" href="#"
-                        aria-expanded="false">
+                    <a class="dropdown-toggle profile-pic" data-toggle="dropdown" href="#" aria-expanded="false">
                         <span
                             class="avatar-sm rounded-circle d-flex justify-content-center align-items-center text-primary font-weight-bold"
                             style="background-color: rgb(202, 202, 202);">
